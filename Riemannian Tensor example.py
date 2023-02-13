f4e5554726cf516fb8b92288d5e5ceb0d182d424
HAN\simon <simonestyx@gmail.com>
2023-02-13 15:20:37 +0000

    commit

--- Riemannian Tensor example.py
+++ Riemannian Tensor example.py
@@ -1,19 +1,19 @@
-import sympy as sp, numpy as np
+import numpy as np
+from scipy.misc import derivative
 
 def Riemann_tensor(g):
     n = g.shape[0]
-    R = sp.zeros(n, n, n, n)
-    x, y = sp.symbols('x y')
-    g_sym = sp.Matrix(g)
-    g_inv = g_sym.inv()
+    R = np.zeros((n, n, n, n))
+    g_inv = np.linalg.inv(g)
     for i in range(n):
         for j in range(n):
             for k in range(n):
                 for l in range(n):
-                    R[i, j, k, l] = sp.diff(sp.diff(g_inv[i, k], x), y) * g_inv[j, l]
-                    + sp.diff(sp.diff(g_inv[i, l], x), y) * g_inv[j, k]
-                    - sp.diff(sp.diff(g_inv[i, j], x), y) * g_inv[k, l]
+                    def f(x):
+                        return g_inv[i, k] * g_inv[j, l] * x
+                    R[i, j, k, l] = derivative(f, 0, dx=1e-6)
     return R
 
-g = np.array([[1, 0], [0, 1]])
+g = np.array([[1, 0], [0, -1]])
 R = Riemann_tensor(g)
+print(R)
